febd3102d32b6ae74af571a499cb0d497e78de99
pancr9 <rpanchal@uncc.edu>
2017-11-05 01:48:13 -0400

    Updated Retrofit

--- app/src/main/java/herapheri/com/fintech/Utils/RetrofitAPI.java
+++ app/src/main/java/herapheri/com/fintech/Utils/RetrofitAPI.java
@@ -4,10 +4,8 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
 import retrofit2.http.POST;
-import retrofit2.http.Query;
+import retrofit2.http.Path;
 
 /**
  * Created by Rekhansh on 11/4/2017.
@@ -18,23 +16,30 @@ import retrofit2.http.Query;
 
 public interface RetrofitAPI {
 
-    // Login
-    @FormUrlEncoded
-    @POST("staffLogin")
-    Call<ResponseBody> doLogin(@Field("userName") String username,
-                               @Field("password") String password);
 
-    //Log Out
-    @GET("logout")
-    Call<ResponseBody> doLogOut(@Query("authToken") String authToken);
+    //Call for Cobrand Login.
+    @FormUrlEncoded
+    @POST("{cobrandName}/login")
+    Call<ResponseBody> doCobrandLogin(@Field("cobrandLogin") String cobrandLogin,
+                                      @Field("cobrandPassword") String cobrandPassword,
+                                      @Path("cobrandName") String cobrandName);
+
+    //Call for user login.
+    @FormUrlEncoded
+    @POST("user/login")
+    Call<ResponseBody> doUserLogin(@Field("loginName") String loginName,
+                                   @Field("password") String password);
+
+
+    //Call for Logout
+    @POST("/{cobrandName}/v1/user/logout")
+    Call<ResponseBody> doLogOut(@Path("cobrandName") String cobrandName);
+
 
     /*
      * GET calls.
      */
     //Call to fetch account and rest details.
-    //http://129.150.95.24:8001/bankAccounts
-    @GET
-    Call<ResponseBody> getAccountDetails(@Header("jSessionId") String jsessionid);
 
 
     /*
@@ -55,7 +60,6 @@ public interface RetrofitAPI {
 
     //Generate Session ID for accessing OBDX services.
     //http://129.150.95.24:8001/access
-
 
 
     //PUT calls.
